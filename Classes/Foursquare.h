7fc3db76cd0a1d79ca43824ed8d4b874fb8b0457
wbison <wbison@attic.nl>
2011-06-26 15:34:00 +0200

    4s

---
+++ Classes/Foursquare.h
@@ -0,0 +1,18 @@
+//
+//  Foursquare.h
+//  HarkPad
+//
+//  Created by Willem Bison on 12-06-11.
+//  Copyright 2011 The Attic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Foursquare : NSObject {
+    
+}
+
+- (id) getResponseFromJson: (NSData *)data;
+
+@end
