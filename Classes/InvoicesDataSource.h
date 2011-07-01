cb462115a7fef6d02918fc8da6420de1450d4f12
wbison <wbison@attic.nl>
2011-07-01 21:41:07 +0200

    missing

---
+++ Classes/InvoicesDataSource.h
@@ -0,0 +1,16 @@
+//
+//  InvoicesDataSource.h
+//  HarkPad
+//
+//  Created by Willem Bison on 01-07-11.
+//  Copyright 2011 The Attic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface InvoicesDataSource : NSObject {
+    
+}
+
+@end
---
+++ Classes/InvoicesDataSource.m
@@ -0,0 +1,14 @@
+//
+//  InvoicesDataSource.m
+//  HarkPad
+//
+//  Created by Willem Bison on 01-07-11.
+//  Copyright 2011 The Attic. All rights reserved.
+//
+
+#import "InvoicesDataSource.h"
+
+
+@implementation InvoicesDataSource
+
+@end
