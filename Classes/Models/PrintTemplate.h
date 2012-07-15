1368652ee10df099436bc6614a5a193e26fc1673
wbison <wbison@attic.nl>
2012-07-15 12:23:03 +0200

    PrintTemplate

---
+++ Classes/Models/PrintTemplate.h
@@ -0,0 +1,12 @@
+//
+// Created by wbison on 15-07-12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface PrintTemplate : NSObject
+@end
---
+++ Classes/Models/PrintTemplate.m
@@ -0,0 +1,15 @@
+//
+// Created by wbison on 15-07-12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "PrintTemplate.h"
+
+
+@implementation PrintTemplate {
+
+}
+
+@end
